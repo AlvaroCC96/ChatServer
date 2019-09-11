539209a6b3f6e8a66a3c297d1e0cb4c18977ae97
AlvaroCC96 <alvaro.castillo@alumnos.ucn.cl>
2019-09-11 20:45:58 -0300

    Class And Libraries for basic chat server

---
+++ src/main/java/ChatMessage.java
@@ -0,0 +1,21 @@
+import java.time.LocalDateTime;
+
+public class ChatMessage {
+    /**
+     * atributes message class
+     * */
+    private final LocalDateTime timestamp;
+    private final String username;
+    private final String message;
+
+    /**
+     * Constructor of class ChatMessage
+     * @param user
+     * @param contentMessage
+     */
+    public ChatMessage(String user,String contentMessage){
+        timestamp = LocalDateTime.now(); //datetime from server clock
+        username= user;
+        message=contentMessage;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+public class Main {
+    /**
+     * port to start the server
+     */
+    private static final int PORT = 9000;
+    /**
+     *Principal
+     */
+    public static void main(final String[] args) throws IOException {
+        ChatServer Server = new ChatServer(PORT); //initialize the server and put it to run
+        Server.start();
+    }
+
+
+}
