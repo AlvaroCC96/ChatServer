96fb45b6989c3b598473051e66cbec83016d84f2
AlvaroCC96 <alvaro.castillo@alumnos.ucn.cl>
2019-09-17 23:37:28 -0300

    Update ChatMessage.java
    documentation chat message

--- src/main/java/cl/ucn/disc/dsm/chat/ChatMessage.java
+++ src/main/java/cl/ucn/disc/dsm/chat/ChatMessage.java
@@ -5,8 +5,8 @@ import java.time.format.DateTimeFormatter;
 public class ChatMessage {
     /**
      * atributes message class
-     * */
-    private final LocalDateTime timestamp;
+     */
+    private final LocalDateTime timestamp; // for record message
     private final String username;
     private final String message;
 
@@ -21,12 +21,14 @@ public class ChatMessage {
         message=contentMessage;
     }
 
+    /**
+     * Function that returns a message with user time date and message
+     * @return message chat formated
+     */
     public String toString(){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         StringBuilder chat = new StringBuilder();
         chat.append(timestamp.format(formatter)).append(" ").append(username).append(" ").append(message);//formated
         return chat.toString(); //create string
     }
-
 }
-
